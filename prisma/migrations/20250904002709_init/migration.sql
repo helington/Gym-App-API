5ba5f900a6c6f8545c626b07f654dfce19a3ce03
helington <willamy.helington@gmail.com>
2025-09-03 21:27:54 -0300

    feat: add database tables into app

---
+++ prisma/migrations/20250904002709_init/migration.sql
@@ -0,0 +1,55 @@
+-- CreateTable
+CREATE TABLE "public"."User" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "public"."Exercise" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "muscle" TEXT NOT NULL,
+    "equipment" TEXT NOT NULL,
+    "difficulty" TEXT NOT NULL,
+    "instructions" TEXT NOT NULL,
+
+    CONSTRAINT "Exercise_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "public"."Workout" (
+    "id" SERIAL NOT NULL,
+    "userId" INTEGER NOT NULL,
+    "date" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "notes" TEXT,
+
+    CONSTRAINT "Workout_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "public"."WorkoutExercise" (
+    "id" SERIAL NOT NULL,
+    "workoutId" INTEGER NOT NULL,
+    "exerciseId" INTEGER NOT NULL,
+    "weight" DOUBLE PRECISION NOT NULL,
+    "reps" INTEGER NOT NULL,
+    "sets" INTEGER NOT NULL,
+
+    CONSTRAINT "WorkoutExercise_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "public"."User"("email");
+
+-- AddForeignKey
+ALTER TABLE "public"."Workout" ADD CONSTRAINT "Workout_userId_fkey" FOREIGN KEY ("userId") REFERENCES "public"."User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "public"."WorkoutExercise" ADD CONSTRAINT "WorkoutExercise_workoutId_fkey" FOREIGN KEY ("workoutId") REFERENCES "public"."Workout"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "public"."WorkoutExercise" ADD CONSTRAINT "WorkoutExercise_exerciseId_fkey" FOREIGN KEY ("exerciseId") REFERENCES "public"."Exercise"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
