2aa3986424c768943fbe13bcfd5208a42eba9bd9
helington <willamy.helington@gmail.com>
2025-09-14 19:30:23 -0300

    feat: improve user's endpoints

--- src/modules/user/user.routes.ts
+++ src/modules/user/user.routes.ts
@@ -1,12 +1,16 @@
 import { Router } from "express";
 import { UserController } from "./user.controller";
-import { createUserSchema } from "./user.schemas";
+import { createUserSchema, updateUserSchema } from "./user.schemas";
 import { validate } from "../../middleware/validate";
+import { authenticate } from "../../middleware/authenticate";
 
 const router = Router();
 
-router.get("/", UserController.getAll);
 router.post("/", validate(createUserSchema), UserController.create);
-router.get("/:id", UserController.getById);
+router.patch("/:userId", authenticate, validate(updateUserSchema), UserController.update);
+router.delete("/:userId", authenticate, UserController.delete);
+
+router.get("/", UserController.getAll);
+router.get("/:userId", UserController.getById);
 
 export default router;
