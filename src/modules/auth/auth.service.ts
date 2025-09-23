fc3283b215816ea42d39b82c1ae21ab030bf8d08
helington <willamy.helington@gmail.com>
2025-09-23 11:59:03 -0300

    feat: add expiration into token

--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -6,6 +6,7 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const JWT_SECRET_KEY = process.env.JWT_SECRET_KEY as string;
+const JWT_ACCESS_EXPIRATION = parseInt(process.env.JWT_ACCESS_EXPIRATION as string);
 
 export const AuthService = {
     loginUser: async (email: string, password: string) => {
@@ -15,6 +16,6 @@ export const AuthService = {
             throw new Error("Invalid credentials!");
         }
 
-        return jwt.sign({id: user.id}, JWT_SECRET_KEY)
+        return jwt.sign({id: user.id}, JWT_SECRET_KEY, { expiresIn: JWT_ACCESS_EXPIRATION })
     },
 };
